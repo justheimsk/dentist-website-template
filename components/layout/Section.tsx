d3f0184d09b85a499b3534b85f5667cdc30dc669
justheimsk <henrique.mdimer@gmail.com>
2024-08-17 15:02:29 -0300

    chore: removed useless props & add support for all div element attributes

--- components/layout/Section.tsx
+++ components/layout/Section.tsx
@@ -1,13 +1,11 @@
-export type SectionProps = {
+export interface SectionProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
-  ignoreNavbar?: boolean;
-  fullHeight?: boolean;
 }
 
 export function Section(props: SectionProps) {
   return (
     <>
-      <section className={`${props.fullHeight ? 'h-screen' : ''} flex items-center ${props.ignoreNavbar ? 'section__ignore-navbar' : ''}`}>{props.children}</section>
+      <section {...props} className={`mt-5 flex flex-col md:flex-row justify-center gap-8 lg:gap-0 lg:items-center ${props.className}`}> {props.children}</section >
     </>
   )
 }
