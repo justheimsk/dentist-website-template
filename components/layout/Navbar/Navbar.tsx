4a412384386d18cd0f64b1d3d6aee14072e99d43
justheimsk <henrique.mdimer@gmail.com>
2024-08-17 15:03:54 -0300

    feat: made navbar responsive

--- components/layout/Navbar/Navbar.tsx
+++ components/layout/Navbar/Navbar.tsx
@@ -1,19 +1,25 @@
 import { Button } from "@/components/ui/button";
 import { MenuLink } from "./MenuLink";
 import { Container } from "../Container";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 export function Navbar() {
   return (
-    <Container className="w-full sticky top-2">
+    <Container className="w-full sticky z-50 top-2">
       <nav className="h-14 mt-3 backdrop-blur-sm bg-opacity-50 bg-gray-200 rounded-full px-5 flex items-center justify-between">
         <div className="bg-blue-500 rounded-full h-8 w-8"></div>
-        <ul className="flex items-center gap-5">
+        <ul className="hidden lg:flex items-center gap-5">
           <MenuLink>Tratamentos</MenuLink>
           <MenuLink>Preços</MenuLink>
           <MenuLink>Sobre nós</MenuLink>
           <MenuLink>Contato</MenuLink>
         </ul>
-        <Button size="sm">Agendar</Button>
+        <div className="flex gap-3 cursor-pointer lg:gap-0 items-center justify-center">
+          <Button size="sm">Agendar</Button>
+          <div className="lg:hidden">
+            <RxHamburgerMenu className="text-3xl" />
+          </div>
+        </div>
       </nav>
     </Container>
   )
