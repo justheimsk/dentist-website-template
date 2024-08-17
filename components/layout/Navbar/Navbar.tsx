7581b1b946e0929472f6016f5d312d9498671477
justheimsk <henrique.mdimer@gmail.com>
2024-08-17 13:36:40 -0300

    feat: make navbar sticky and blur bg

--- components/layout/Navbar/Navbar.tsx
+++ components/layout/Navbar/Navbar.tsx
@@ -4,8 +4,8 @@ import { Container } from "../Container";
 
 export function Navbar() {
   return (
-    <Container>
-      <nav className="h-14 flex items-center justify-between">
+    <Container className="w-full sticky top-2">
+      <nav className="h-14 mt-3 backdrop-blur-sm bg-opacity-50 bg-gray-200 rounded-full px-5 flex items-center justify-between">
         <div className="bg-blue-500 rounded-full h-8 w-8"></div>
         <ul className="flex items-center gap-5">
           <MenuLink>Tratamentos</MenuLink>
